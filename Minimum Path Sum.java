4a951fc19388ad12e5a2ce985d4fbc187bdb8fa8
Dhwaj Sharma <dhwajsharma10@gmail.com>
2021-04-11 15:50:17 +0530

    https://leetcode.com/problems/minimum-path-sum/

---
+++ Minimum Path Sum.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minPathSum(int[][] grid) {
+        int height = grid.length;
+        int width = grid[0].length;
+        
+        for(int row=0; row < height; row++){
+            for(int col=0; col < width; col++){
+                if(row == 0 && col == 0) grid[row][col] = grid[row][col];
+                if(row != 0 && col == 0) grid[row][col] = grid[row][col] + grid[row-1][col];
+                if(row == 0 && col != 0) grid[row][col] = grid[row][col] + grid[row][col-1];
+                if(row != 0 && col != 0) grid[row][col] = grid[row][col] + Math.min(grid[row][col-1], grid[row-1][col]);
+            }
+        }
+        return grid[height-1][width-1];
+    }
+}
