72bdd4e9399d91f2c6ccfd97183eb919cc8f01df
Dhwaj Sharma <54977261+dhwajsharma@users.noreply.github.com>
2021-05-01 08:54:41 +0530

    https://leetcode.com/problems/valid-anagram/

--- Valid Anagram.java
+++ Valid Anagram.java
@@ -5,7 +5,6 @@ class Solution {
         for(int i=0; i < t.length(); i++) alphabet[t.charAt(i) - 'a']--;
         for(int i: alphabet)
             if(i != 0) return false;
-            
         return true;
     }
 }
