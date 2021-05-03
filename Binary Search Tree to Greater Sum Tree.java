7cea1a210010e76921732888bd1d935271fc9cf7
Dhwaj Sharma <54977261+dhwajsharma@users.noreply.github.com>
2021-05-03 21:20:21 +0530

    https://leetcode.com/problems/binary-search-tree-to-greater-sum-tree/

---
+++ Binary Search Tree to Greater Sum Tree.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int pre = 0;
+    public TreeNode bstToGst(TreeNode root) {
+        if(root.right != null) bstToGst(root.right);
+        pre = root.val = root.val + pre;
+        if(root.left != null) bstToGst(root.left);
+        
+        return root;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
