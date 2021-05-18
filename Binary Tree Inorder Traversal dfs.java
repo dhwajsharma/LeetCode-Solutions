558d58b37a0134ddc9fb1f13510185e65887d047
Dhwaj Sharma <54977261+dhwajsharma@users.noreply.github.com>
2021-05-18 16:09:16 +0530

    https://leetcode.com/problems/binary-tree-inorder-traversal/

---
+++ Binary Tree Inorder Traversal dfs.java
@@ -0,0 +1,59 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        dfs(root, res);
+        return res;
+    }
+    
+    private void dfs(TreeNode node, List<Integer> res){
+        if(node != null){
+        dfs(node.left, res);
+        res.add(node.val);
+        dfs(node.right, res);    
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
