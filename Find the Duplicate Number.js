ab79e687e3ef53ee1a67a4894e11c3be20f4a6b7
Dhwaj Sharma <54977261+dhwajsharma@users.noreply.github.com>
2021-05-23 19:20:06 +0530

    https://leetcode.com/problems/find-the-duplicate-number/

---
+++ Find the Duplicate Number.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findDuplicate = function(nums) {
+    let slow = nums[0];
+    let fast = nums[nums[0]];
+    
+    while(slow != fast){
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+    }
+    
+    slow = 0;
+    
+    while(slow != fast){
+        slow = nums[slow];
+        fast = nums[fast];
+    }
+    
+    return slow;
+};
