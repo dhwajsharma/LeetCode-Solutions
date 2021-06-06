1b2f085273d33a316bcf3e8a316c47db664454ae
Dhwaj Sharma <54977261+dhwajsharma@users.noreply.github.com>
2021-06-06 22:47:52 +0530

    https://leetcode.com/problems/number-of-students-doing-homework-at-a-given-time/

---
+++ Number of Students Doing Homework at a Given Time.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} startTime
+ * @param {number[]} endTime
+ * @param {number} queryTime
+ * @return {number}
+ */
+var busyStudent = function(startTime, endTime, queryTime) {
+    let res = 0;
+    for(let i = 0; i < startTime.length; i++){
+        startTime[i] <= queryTime && endTime[i] >= queryTime && res++;
+    }
+    return res;
+};
