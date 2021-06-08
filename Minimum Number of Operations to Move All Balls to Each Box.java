3191d4558c3d7a8a37cc11ffefd533ddaeee23bb
Dhwaj Sharma <54977261+dhwajsharma@users.noreply.github.com>
2021-06-08 23:43:54 +0530

    https://leetcode.com/problems/minimum-number-of-operations-to-move-all-balls-to-each-box/

---
+++ Minimum Number of Operations to Move All Balls to Each Box.java
@@ -0,0 +1,14 @@
+public int[] minOperations(String boxes) {
+    int[] res = new int[boxes.length()];
+    for (int i = 0, ops = 0, cnt = 0; i < boxes.length(); ++i) {
+       res[i] += ops;
+       cnt += boxes.charAt(i) == '1' ? 1 : 0;
+       ops += cnt;
+    }    
+    for (int i = boxes.length() - 1, ops = 0, cnt = 0; i >= 0; --i) {
+        res[i] += ops;
+        cnt += boxes.charAt(i) == '1' ? 1 : 0;
+        ops += cnt;
+    }
+    return res;
+}
