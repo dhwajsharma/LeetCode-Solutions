9e577f023ddaaff2d31a51b5eabdc7571809258f
Dhwaj Sharma <54977261+dhwajsharma@users.noreply.github.com>
2021-07-09 08:32:09 +0530

    https://leetcode.com/problems/binary-tree-postorder-traversal/

---
+++ Binary Tree Postorder Traversal.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var postorderTraversal = function(root) {
+    if(!root) return [];
+    const stack = [root];
+    const result = [];
+    
+    while(stack.length > 0){
+        const node = stack.pop();
+        result.push(node.val);
+        if(node.left) stack.push(node.left);
+        if(node.right) stack.push(node.right);
+    }
+    return result.reverse();
+};
+
+
+
+
+
+
+
+
+
+
