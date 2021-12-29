0849a37d59dba10cc14e4f417848e0a9fe93a1e5
Dhwaj Sharma <dhwajsharma10@gmail.com>
2021-12-29 23:33:02 +0530

    https://leetcode.com/problems/remove-nth-node-from-end-of-list/

---
+++ Remove Nth Node From End of List.js
@@ -0,0 +1,16 @@
+var removeNthFromEnd = function(head, n) {
+    let hare = head, curr = head;
+    while (n--) {
+        hare = hare.next;
+    }
+    while (hare && hare.next) {
+        curr = curr.next;
+        hare = hare.next;
+    }
+    if (!hare) {
+        head = head.next;
+    } else {
+        curr.next = curr.next ? curr.next.next : null;
+    }
+    return head;
+};
